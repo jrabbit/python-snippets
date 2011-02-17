07987d187283ad22684696158540ce3e97f0ce37
Jrabbit <jackjrabbit+github@gmail.com>
2011-02-17 16:04:59 -0600

    now works on stock OSX.

--- foxwallpaper.py
+++ foxwallpaper.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import re
 import platform
 from subprocess import *
 import feedparser
@@ -10,10 +11,10 @@ from BeautifulSoup import BeautifulSoup
 
 from appscript import app, mactypes
 
-def get_wallpapers():
+def get_wallpapers(entry=0):
     feed = "http://thefoxisblack.com/category/the-desktop-wallpaper-project/feed/"
     d = feedparser.parse(feed)
-    soup = BeautifulSoup(d.entries[0].content[0].value)
+    soup = BeautifulSoup(d.entries[entry].content[0].value)
     urls = {}
     for x in soup.findAll('a'):
         if  x['href'].split('-')[-1][-3:] == "jpg":
@@ -36,9 +37,11 @@ def directory(name="foxwallpaper"):
         os.mkdir(app_dir)
     return app_dir
 
+
 def get_db():
     db = anydbm.open(os.path.join(directory(), 'settings'), 'c')
     return db
+
 def is_first_run():
     if 'size' not in get_db():
         return True
@@ -48,15 +51,24 @@ def is_first_run():
 def download(url):
     Popen(['wget', '-c', url], cwd=directory()).communicate()
     return os.path.join(directory(), url.split('/')[-1])
+
 def get_resolution():
-    out = Popen(['xrandr'], stdout=PIPE, stderr=PIPE).communicate()[0]
-    real = out.split('\n')[0].split(',')[-1].split()
-    return real[1] + 'x' + real[3]
-    
+    try:
+        #Prefered xrandr method. Broken on stock Apple X11.
+        out = Popen(['xrandr'], stdout=PIPE, stderr=PIPE).communicate()[0]
+        real = out.split('\n')[0].split(',')[-1].split()
+        return real[1] + 'x' + real[3]
+    # /usr/sbin/system_profiler SPDisplaysDataType | grep Resolution
+    except IndexError:
+        profiler = Popen(['system_profiler', 'SPDisplaysDataType'],
+        stdout=PIPE, stderr=PIPE).communicate()[0]
+        expr = re.compile('Resolution')
+        resolution = filter(expr.search, profiler.splitlines())[0]
+        return resolution.split()[1] + 'x' + resolution.split()[3]
+
 def set_desktop_mac(path):
     # /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture "/path/to/the picture.jpg"
     app('Finder').desktop_picture.set(mactypes.File(path))
-
 
 if __name__ == '__main__':
     db = get_db()
