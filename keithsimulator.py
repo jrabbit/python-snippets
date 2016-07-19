c81cd9346ba8a1e830ea5d03dcccc5dd4969ee14
jrabbit <jackjrabbit@gmail.com>
2016-07-19 11:26:39 -0400

    pep 8 the shirt out of that

--- keithsimulator.py
+++ keithsimulator.py
@@ -1,14 +1,17 @@
 import time
-daynum=int(time.strftime('%j'))
-year=int(time.strftime('%y'))
-composite=(year*365)+daynum
+daynum = int(time.strftime('%j'))
+year = int(time.strftime('%y'))
+composite = (year*365)+daynum
+
+
 def ordinal(n):
-     if 10 <= n % 100 < 20:
-         return str(n) + 'th'
-     else:
-        return  str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, "th")
-    #thanks http://stackoverflow.com/questions/739241/python-date-ordinal-output/739301#739301
+    if 10 <= n % 100 < 20:
+        return str(n) + 'th'
+    else:
+        return str(n) + {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, "th")
+    # thanks
+    # http://stackoverflow.com/questions/739241/python-date-ordinal-output/739301#739301
 print "It is the %s day since President Bush declared mission accomplished in Iraq, \
 the %s day since he declared victory in Afghanistan, \
-and %s day of the Deepwater Horizon disaster in the Gulf."% (ordinal(composite - 1214), ordinal(composite - 1625), ordinal(composite - 3759))
-#Composite dates - date of occurance for July 8th -- 2625 Iraq 2214 Afg 80 Oil
+and %s day of the Deepwater Horizon disaster in the Gulf." % (ordinal(composite - 1214), ordinal(composite - 1625), ordinal(composite - 3759))
+# Composite dates - date of occurance for July 8th -- 2625 Iraq 2214 Afg 80 Oil
